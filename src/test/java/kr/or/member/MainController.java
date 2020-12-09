93bfd0a8bf0f007d44f8c731e5721fd646902a46
user <user@JY-20200220PDEN>
2020-12-09 11:45:05 +0900

    20201209 3교시 커밋

--- src/test/java/kr/or/member/MainController.java
+++ src/test/java/kr/or/member/MainController.java
@@ -1,11 +1,23 @@
 package kr.or.member;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
+
 import kr.or.member.MemberService;
 import kr.or.member.MemberVO;
 
 public class MainController {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// 실행메서드
 		MemberVO memberVO = new MemberVO();
 		memberVO.setName("홍길동");
@@ -28,6 +40,29 @@ public class MainController {
 		MemberService memberService = new MemberService();// 자바 오브젝트객체 생성하는방법
 		memberService.printMember(members);// 서비스클래스의 메서드호출
 
+		//서버시간 가져오기 코딩(아래)
+		//서버시간을 가져올 수 있는 commons-net 메이븐 디펜던시 사용 라이브러리
+		NTPUDPClient timeClient = new NTPUDPClient();
+		timeClient.setDefaultTimeout(1000);
+		//네트워크로 서버시간을 가져올래, 응답이 1초를 넘기면, 재접속해라.
+		timeClient.open();
+		//pool.ntp.org는 서버시간보내줄 실제 서버주소.
+		InetAddress address = InetAddress.getByName("pool.ntp.org");
+		TimeInfo timeInfo = timeClient.getTime(address);
+		//서버시간을 담은 timeInfo 객체변수를 사용(아래) 변수는 카멜표기법(낙타등표기법)
+		//TimeStamp 1970년부터 초단위로 계산된 현재까지의 초를 합친결과값(아래) 예,13143545115431초
+		long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();
+		//위 반환받은 타임스탬프초 값을 사람이 알아볼 수 있는 시간으로 변환하기(아래)
+		Date nowDate = new Date(returnTime);
+		System.out.println("현재 서버시간은 " + nowDate);
+		//Wed Dec 09 11:19:26 KST 2020 -> 2020/12/09 11:19:26 AM
+		//SimpleDateFormat formatDate = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
+		//System.out.println("현재 서버시간은 - 한국버전" + formatDate.format(nowDate));
+		LocalDateTime localDateTime = nowDate.toInstant()
+				.atZone(ZoneId.systemDefault())
+				.toLocalDateTime();
+		System.out.println("Server 시간" + localDateTime);
+		System.out.println("로컬PC 시간" + localDateTime.now());
 	}
-
+	
 }
--- src/test/java/kr/or/test/Step3.java
+++ src/test/java/kr/or/test/Step3.java
@@ -21,22 +21,31 @@ public class Step3 {
 		int intWeek = calendar.get(Calendar.DAY_OF_WEEK);
 		switch(intWeek) {
 		case 1:
-			today = Week.월; break;//브레이크는 case문장을 빠져나가는 명령
-		case 2:
-			today = Week.화; break;//Week자료형에 바로접근하는 방법 Week.
-		case 3:
-			today = Week.수; break;
-		case 4:
-			today = Week.목; break;
-		case 5:
-			today = Week.금; break;
-		case 6:
-			today = Week.토; break;
-		case 7:
 			today = Week.일; break;
+		case 2:
+			today = Week.월; break;//브레이크는 case문장을 빠져나가는 명령
+		case 3:
+			today = Week.화; break;//Week자료형에 바로접근하는 방법 Week.
+		case 4:
+			today = Week.수; break;
+		case 5:
+			today = Week.목; break;
+		case 6:
+			today = Week.금; break;
+		case 7:
+			today = Week.토; break;
+	
 		}
 
-		System.out.println("오늘 요일은: " + today + "요일입니다.");
+		System.out.println("PC의 오늘 요일은: " + today + "요일입니다.");
+		
+		if(today == Week.수) {//이번에는 if조건문을 사용.
+			System.out.println("수요일에는 축구를 합니다.");
+		} else {
+			System.out.println("열심히 자바공부를 합니다.");
+		}
+		//Calendar클래스는 달력에서 날짜, 본인 컴퓨터 날짜라서 데이터가 훼손된 가능성이 있습니다.
+		//이때 해결책은 서버시간을 가져와서 출력하면, 사용자가 날짜를 수정 못하게 됩니다.
 	}
 
 }
